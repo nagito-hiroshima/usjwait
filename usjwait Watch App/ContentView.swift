fc07f67cf02a49571b49364b413438e63a6a5772
なぎと-nagito- <hello@nagito.work>
2025-11-04 02:42:03 +0900

    Initial Commit

---
+++ usjwait Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  usjwait Watch App
+//
+//  Created by 吉田 文太 on 2025/11/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ usjwait Watch App/usjwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  usjwaitApp.swift
+//  usjwait Watch App
+//
+//  Created by 吉田 文太 on 2025/11/04.
+//
+
+import SwiftUI
+
+@main
+struct usjwait_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
